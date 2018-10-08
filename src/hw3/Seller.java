7aa9ab2b60e5f08919d16e7c5b3dc74696630509
goyaldarpan <lucifer.xfear@gmail.com>
2018-10-08 16:40:44 -0700

    More than 1 hour check

--- src/hw3/Seller.java
+++ src/hw3/Seller.java
@@ -9,107 +9,110 @@ import java.util.Random;
 
 public abstract class Seller implements Runnable
 {
-    public Queue<Customer> customerQueue;
-    protected static Random randomVariable = new Random();
-    protected String sellerID;
-    protected int serviceTime;
-    protected int ticketNum = 1;
-    protected int time = 0;
+	public Queue<Customer> customerQueue;
+	protected static Random randomVariable = new Random();
+	protected String sellerID;
+	protected int serviceTime;
+	protected int ticketNum = 1;
+	protected int time = 0;
 
-    protected long pastTime;
-    protected long currentTime;
+	protected long pastTime;
+	protected long currentTime;
 
-    protected Seat[][] seating;
-    private Object lock;
+	protected Seat[][] seating;
+	private Object lock;
 
-    public Seller(Seat[][] seating, int serviceTime, String sellerID, Object lock, long pastTime)
-    {
-        customerQueue = new LinkedList<Customer>();
-        this.serviceTime = serviceTime;
-        this.seating = seating;
-        this.lock = lock;
-        this.sellerID = sellerID;
-        this.pastTime = pastTime;
+	public Seller(Seat[][] seating, int serviceTime, String sellerID, Object lock, long pastTime)
+	{
+		customerQueue = new LinkedList<Customer>();
+		this.serviceTime = serviceTime;
+		this.seating = seating;
+		this.lock = lock;
+		this.sellerID = sellerID;
+		this.pastTime = pastTime;
 
-    }
-    protected void callTime(Customer customer)
-    {
+	}
+	protected void callTime(Customer customer)
+	{
 
-        time = (int) (currentTime + serviceTime); //+ elapse_time;
-        System.out.println("Arrival time: " + customer.getArrivalTime() + "min");
-        System.out.println("Seller ID: " + this.sellerID);
-        System.out.println("Time after arrival to be served: " + this.serviceTime + "min");
-        customer.setTime(time);
-    }
+		time = (int) (currentTime + serviceTime); //+ elapse_time;
+		System.out.println("Arrival time: " + customer.getArrivalTime() + "min");
+		System.out.println("Seller ID: " + this.sellerID);
+		System.out.println("Time after arrival to be served: " + this.serviceTime + "min");
+		customer.setTime(time);
+	}
 
-    protected void assignSeat(Customer customer, Seat seat, int i, int j)
-    {
-        if (ticketNum < 10)
-            customer.setTicket(sellerID + "0" + ticketNum);
-        else
-            customer.setTicket(sellerID + ticketNum);
-        callTime(customer);
-        ticketNum++;
-        seat.assignSeat(customer);
-        seating[i][j] = seat;
-    }
+	protected void assignSeat(Customer customer, Seat seat, int i, int j)
+	{
+		if (ticketNum < 10)
+			customer.setTicket(sellerID + "0" + ticketNum);
+		else
+			customer.setTicket(sellerID + ticketNum);
+		callTime(customer);
+		ticketNum++;
+		seat.assignSeat(customer);
+		seating[i][j] = seat;
+	}
 
-    protected void update()
-    {
+	protected void update()
+	{
 
-        currentTime = System.currentTimeMillis() - this.pastTime;
-        if (currentTime < 1000)
-            currentTime = 0;
-        else
-            currentTime /= 1000;
-    }
+		currentTime = System.currentTimeMillis() - this.pastTime;
+		if (currentTime < 1000)
+			currentTime = 0;
+		else
+			currentTime /= 1000;
+	}
 
-    public void addCustomer(Customer c) {
-        customerQueue.add(c);
-    }
+	public void addCustomer(Customer c) {
+		customerQueue.add(c);
+	}
 
-    public void sortQueue()
-    {
-        Customer[] temp = customerQueue.toArray(new Customer[customerQueue.size()]);
-        customerQueue.clear();
-        Arrays.sort(temp);
-        for (Customer c : temp)
-            customerQueue.add(c);
-    }
+	public void sortQueue()
+	{
+		Customer[] temp = customerQueue.toArray(new Customer[customerQueue.size()]);
+		customerQueue.clear();
+		Arrays.sort(temp);
+		for (Customer c : temp)
+			customerQueue.add(c);
+	}
 
-    protected void printMsg(Customer customer, Seat seat)
-    {
-        int hour = customer.getTime() / 60;
-        int min = customer.getTime() % 60;
-        String time = "";
-        if (min < 10) time = hour + ":0" + min;
-        else time = hour + ":" + min;
-        if (seat == null) System.out.println(time + "  " + sellerID + " - Sorry, the concert is sold out!");
-        else System.out.println(time + "  " + sellerID + " - Success! Your seat is " + seat.getSeatNumber());
-        
-        printSeating(this.seating, 10, 10);
-    }
+	protected void printMsg(Customer customer, Seat seat)
+	{
+		int hour = customer.getTime() / 60;
+		int min = customer.getTime() % 60;
+		String time = "";
+		if (min < 10) time = hour + ":0" + min;
+		else time = hour + ":" + min;
+		if (hour > 0 || seat == null) System.out.println(time + "  " + sellerID + " - Sorry, The tickets aren't available anymore!");
+		else 
+		{
+			System.out.println(time + "  " + sellerID + " - Success! Your seat is " + seat.getSeatNumber());
+			printSeating(this.seating, 10, 10);
+		}
 
-    // seller thread to serve one time "quanta" á 1 minute
+	}
 
-    public abstract void sell();
+	// seller thread to serve one time "quanta" á 1 minute
 
-    @Override
-    public void run() {
-        sell();
-    }
+	public abstract void sell();
 
-    public static void printSeating(Seat[][] seating, int maxRows, int maxCols)
-    {
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        for (int row = 0; row < maxRows; row++) {
-            for (int col = 0; col < maxCols; col++) {
-                if (seating[row][col].isSeatEmpty())
-                    System.out.printf("%7s ", "-");
-                else
-                    System.out.printf("%7s ", seating[row][col].getCustomer().getTicket());
-            }
-            System.out.println();
-        }
-    }
+	@Override
+	public void run() {
+		sell();
+	}
+
+	public static void printSeating(Seat[][] seating, int maxRows, int maxCols)
+	{
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		for (int row = 0; row < maxRows; row++) {
+			for (int col = 0; col < maxCols; col++) {
+				if (seating[row][col].isSeatEmpty())
+					System.out.printf("%7s ", "-");
+				else
+					System.out.printf("%7s ", seating[row][col].getCustomer().getTicket());
+			}
+			System.out.println();
+		}
+	}
 }
